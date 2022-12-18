3fab4824e24d0c664c7a111028b2d59e3a2ae7c7
Dmitry White <dmitry.b.white@gmail.com>
2022-12-18 16:17:25 +0000

    Add null data source to null providers example

--- terraform-troubleshooting/null_provider/main.tf
+++ terraform-troubleshooting/null_provider/main.tf
@@ -17,14 +17,19 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = var.instance_ami
-  instance_type = "t3.micro"
   subnet_id     = var.subnet_id
+  instance_type = data.null_data_source.ec2_values.inputs["ec2_instance_type"]
 
   tags = {
     Name = var.instance_name
   }
 }
 
+# The "null_resource" resource implements the standard resource lifecycle
+# but takes no further action.
+# The "triggers" argument allows specifying an arbitrary set of values
+# that, when changed, will cause the resource to be replaced.
+# Essentially, it's a dependency list similar to "useEffect" hook's dependencies in React.js.
 resource "null_resource" "ec2_status" {
   provisioner "local-exec" {
     command = "./scripts/health.sh"
--- terraform-troubleshooting/null_provider/variables.tf
+++ terraform-troubleshooting/null_provider/variables.tf
@@ -15,3 +15,12 @@ variable "instance_name" {
   type        = string
   default     = "TheFastestManAlive"
 }
+
+# Historically, the "null_data_source" was typically used to
+# construct intermediate values to re-use elsewhere in configuration.
+# The same can now be achieved using locals.
+data "null_data_source" "ec2_values" {
+  inputs = {
+    ec2_instance_type = "t3.micro"
+  }
+}
