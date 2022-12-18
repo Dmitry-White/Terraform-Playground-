e7fc7b5d43c132ac05feddd21da67c352589007c
Dmitry White <dmitry.b.white@gmail.com>
2022-12-18 16:44:34 +0000

    Refactor looping errors implementation

--- terraform-troubleshooting/looping-errors/main.tf
+++ terraform-troubleshooting/looping-errors/main.tf
@@ -1,57 +1,51 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.24.1"
-    }
-  }
-  required_version = "~> 1.0"
-}
-
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_instance" "web_app" {
-  for_each               = aws_security_group.*.id
-  ami                    = "<DUMMY VALUE>"
-  subnet_id              = "<DUMMY VALUE>"
-  instance_type          = "t3.micro"
+  for_each = aws_security_group.*.id
+
+  instance_type = "t3.micro"
+
+  ami                    = local.image.ami_id
+  subnet_id              = local.network.subnet_cidr
   vpc_security_group_ids = [each.id]
-  user_data              = <<-EOF
+
+  user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+
   tags = {
     Name = "${var.name}-justice"
   }
 }
 
 resource "aws_security_group" "sg_ping" {
-   name                      = "Allow Ping"
-   vpc_id                    = "<DUMMY VALUE>"
+  name   = "Allow Ping"
+  vpc_id = local.network.full_cidr
 }
 
 resource "aws_security_group" "sg_8080" {
-   name                      = "Allow 8080"
-   vpc_id                    = "<DUMMY VALUE>"
+  name   = "Allow 8080"
+  vpc_id = local.network.full_cidr
 }
 
- resource "aws_security_group_rule" "sg_ping" {
-   type                      = "ingress"
-   from_port                 = -1
-   to_port                   = -1
-   protocol                  = "icmp"
-   security_group_id         = aws_security_group.sg_ping.id
-   source_security_group_id  = aws_security_group.sg_8080.id
+resource "aws_security_group_rule" "sg_ping" {
+  type     = "ingress"
+  protocol = "icmp"
+
+  from_port = -1
+  to_port   = -1
+
+  security_group_id        = aws_security_group.sg_ping.id
+  source_security_group_id = aws_security_group.sg_8080.id
 }
 
- resource "aws_security_group_rule" "sg_8080" {
-   type                     = "ingress"
-   from_port                = 8080
-   to_port                  = 8080
-   protocol                 = "tcp"
-   security_group_id        = aws_security_group.sg_8080.id
-   source_security_group_id = aws_security_group.sg_ping.id
+resource "aws_security_group_rule" "sg_8080" {
+  type     = "ingress"
+  protocol = "tcp"
+
+  from_port = 8080
+  to_port   = 8080
+
+  security_group_id        = aws_security_group.sg_8080.id
+  source_security_group_id = aws_security_group.sg_ping.id
 }
--- terraform-troubleshooting/looping-errors/variables.tf
+++ terraform-troubleshooting/looping-errors/variables.tf
@@ -1,7 +1,18 @@
-variable region {
+variable "region" {
   description = "The AWS region your resources will be deployed"
 }
 
-variable name {
+variable "name" {
   description = "The operator name running this configuration"
+}
+
+
+locals {
+  network = {
+    full_cidr   = "10.0.0.0/16"
+    subnet_cidr = "10.0.1.0/24"
+  }
+  image = {
+    ami_id = "ami-076309742d466ad69"
+  }
 }
