a1a1093b75e348dfa4ce6d9c0a6a035112c661db
Dmitry White <dmitry.b.white@gmail.com>
2022-12-18 13:57:31 +0000

    Add cycle errors initial setup

--- terraform-troubleshooting/interpolation-error/main.tf
+++ terraform-troubleshooting/interpolation-error/main.tf
@@ -13,15 +13,49 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_app" {
-  ami                    = <DUMMY VALUE>
-  subnet_id              = <DUMMY VALUE>
+  for_each               = local.security_groups
+  
+  ami                    = "<DUMMY VALUE>"
+  subnet_id              = "<DUMMY VALUE>"
   instance_type          = "t3.micro"
+  vpc_security_group_ids = [each.value]
   user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
   tags = {
-    Name = $var.name-justice
+    Name = $var.name-justice-$each.key
+  }
+}
+
+ resource "aws_security_group" "sg_ping" {
+   name = "Allow Ping"
+   vpc_id = "<DUMMY VALUE>"
+
+  ingress {
+    from_port       = -1
+    to_port         = -1
+    protocol        = "icmp"
+    security_groups = [aws_security_group.sg_8080.id]
+  }
+}
+
+ resource "aws_security_group" "sg_8080" {
+   name = "Allow 8080"
+   vpc_id = "<DUMMY VALUE>"
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_ping.id]
+  }
+}
+
+locals {
+  security_groups = {
+    sg_ping   = aws_security_group.sg_ping.id,
+    sg_8080   = aws_security_group.sg_8080.id,
   }
 }
