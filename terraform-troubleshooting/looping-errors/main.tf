f6cc45a0cc06f6ecc7fc71061580797c83c28325
Dmitry White <dmitry.b.white@gmail.com>
2022-12-18 16:59:36 +0000

    Refactor user data for looping errors implementation

--- terraform-troubleshooting/looping-errors/main.tf
+++ terraform-troubleshooting/looping-errors/main.tf
@@ -1,31 +1,27 @@
 resource "aws_instance" "web_app" {
-  for_each = aws_security_group.*.id
+  for_each = local.security_groups
 
   instance_type = "t3.micro"
 
   ami                    = local.image.ami_id
-  subnet_id              = local.network.subnet_cidr
-  vpc_security_group_ids = [each.id]
+  subnet_id              = local.network.subnet_id
+  vpc_security_group_ids = [each.value]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+  user_data = "${file("user-data.sh")}"
 
   tags = {
-    Name = "${var.name}-justice"
+    Name = "${var.name}-justice-${each.key}"
   }
 }
 
 resource "aws_security_group" "sg_ping" {
   name   = "Allow Ping"
-  vpc_id = local.network.full_cidr
+  vpc_id = local.network.vpc_id
 }
 
 resource "aws_security_group" "sg_8080" {
   name   = "Allow 8080"
-  vpc_id = local.network.full_cidr
+  vpc_id = local.network.vpc_id
 }
 
 resource "aws_security_group_rule" "sg_ping" {
