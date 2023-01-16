0cbc491c11a5db746dbc8af4164f313bf6706fcd
Dmitry White <dmitry.b.white@gmail.com>
2023-01-16 12:42:20 +0000

    Add Learning TF network custom implementation

--- learning-terraform/main.tf
+++ learning-terraform/main.tf
@@ -1,24 +1,52 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [
+    aws_security_group.web.id
+  ]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "Allow HTTP/S in. Allow everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_https_in" {
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_http_in" {
+  type      = "ingress"
+  from_port = 0
+  to_port   = 0
+
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
 }
--- learning-terraform/variables.tf
+++ learning-terraform/variables.tf
@@ -1,3 +1,23 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
