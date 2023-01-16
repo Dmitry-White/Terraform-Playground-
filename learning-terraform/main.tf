6f38c5bdbce158e0d8086bb4cf829b2a6b9db9b2
Dmitry White <dmitry.b.white@gmail.com>
2023-01-16 13:02:05 +0000

    Add Learning TF network module implementation

--- learning-terraform/main.tf
+++ learning-terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [
-    aws_security_group.web.id
+    module.web_sg.security_group_id
   ]
 
   tags = {
@@ -11,42 +11,16 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow HTTP/S in. Allow everything out"
+module "web_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  vpc_id = data.aws_vpc.default.id
-}
+  name        = "web_new"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = data.aws_vpc.default.id
 
-resource "aws_security_group_rule" "web_http_in" {
-  type      = "ingress"
-  from_port = 80
-  to_port   = 80
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type      = "ingress"
-  from_port = 443
-  to_port   = 443
-
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_http_in" {
-  type      = "ingress"
-  from_port = 0
-  to_port   = 0
-
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
