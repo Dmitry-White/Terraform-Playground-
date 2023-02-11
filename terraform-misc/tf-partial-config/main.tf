adb3038ab272efc4871f26e15be7320dd2358728
Dmitry White <dmitry.b.white@gmail.com>
2023-02-11 16:29:19 +0000

    Add TF Misc tf-partial-config infra setup

---
+++ terraform-misc/tf-partial-config/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "./modules/vpc"
+  region = var.region
+
+  vpc_range    = var.vpc_range
+  subnet_range = var.subnet_range
+}
+
+resource "aws_instance" "webserver" {
+  ami           = module.vpc.ami_id
+  subnet_id     = module.vpc.subnet_id
+  instance_type = var.instance_type
+
+  tags = {
+    "Name" = "Webserver"
+  }
+}
---
+++ terraform-misc/tf-partial-config/modules/vpc/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  type        = string
+  description = "Deployment Region"
+  default     = "us-east-1"
+}
+
+variable "vpc_range" {
+  type        = string
+  description = "VPC IP Range"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_range" {
+  type        = string
+  description = "Subnet IP Range"
+  default     = "10.0.1.0/24"
+}
+
+data "aws_ssm_parameter" "webserver-ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
