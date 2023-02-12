b0a8aea6158d4e2688036d8e68aa4a637904edda
Dmitry White <dmitry.b.white@gmail.com>
2023-02-12 18:45:21 +0000

    Add TF Pipelines GH Actions initial setup

---
+++ terraform-pipelines/github-actions/variables.tf
@@ -0,0 +1,29 @@
+variable "environment" {
+  type        = string
+  description = "Deployment Environment"
+  default     = "dev"
+}
+
+variable "region" {
+  type        = string
+  description = "Deployment Region"
+  default     = "eu-central-1"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Deployment Instance Type"
+  default     = "t3.micro"
+}
+
+variable "vpc_range" {
+  type        = string
+  description = "Deployment VPC CIDR"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_range" {
+  type        = string
+  description = "Deployment Subnet CIDR"
+  default     = "10.0.1.0/24"
+}
