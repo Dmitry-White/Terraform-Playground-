592f9209cf67576d94ff6540a22030b8e7e2e71d
Dmitry White <dmitry.b.white@gmail.com>
2024-08-26 23:33:19 +0100

    Update Minecraft Server user data script

--- minecraft-server/scripts/user_data.sh
+++ minecraft-server/scripts/user_data.sh
@@ -1,11 +1,49 @@
 #!/bin/bash -e
 
-echo "Preparing server..."
-apt update -y
-apt install -y \
-    openjdk-21-jre \
-    awscli \
-    zip
-echo "User: $tf_node_user."
-echo "Home: $tf_home_path."
-echo "Preparation Successful!"
+SERVER_BUCKET=${tf_server_bucket}
+SERVER_FOLDER=${tf_server_bucket_game_folder}
+GAME_SERVER_RAM=${tf_server_ram}
+
+JAVA_PACKAGE=openjdk-21-jre
+GAME_ARCHIVE_NAME=minecraft
+GAME_ARCHIVE_PATH=$GAME_ARCHIVE_NAME.zip
+
+prepare() {
+    echo "Preparing server..."
+
+    apt update -y
+    apt install -y \
+        $JAVA_PACKAGE \
+        awscli \
+        zip
+
+    echo "Preparation Successful!"
+}
+
+download() {
+    echo "Downloading game files..."
+
+    aws s3 cp s3://$SERVER_BUCKET/$SERVER_FOLDER/$GAME_ARCHIVE_PATH .
+
+    echo "Download Successful!"
+}
+
+extract() {
+    echo "Extracting game files..."
+
+    unzip $GAME_ARCHIVE_PATH
+
+    echo "Extraction Successful!"
+}
+
+run() {
+    echo "Running game server..."
+
+    cd $GAME_ARCHIVE_NAME/server
+    java -Xms$GAME_SERVER_RAM -Xmx$GAME_SERVER_RAM -jar server.jar nogui
+}
+
+prepare
+download
+extract
+run
